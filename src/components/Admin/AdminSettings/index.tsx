8135208f9e3890650d10a3d7f079d992abc1fde7
Karl Romet Somelar <108818570+karlromets@users.noreply.github.com>
2025-04-04 21:11:14 +0300

    refactor(AdminSettings): migrate to TypeScript

--- src/components/Admin/AdminSettings/index.tsx
+++ src/components/Admin/AdminSettings/index.tsx
@@ -1,9 +1,17 @@
 import "@/i18n/i18n";
 import ThemeSwitcher from "@/components/Admin/ThemeSwitcher";
+import { Game } from "@/src/types/game";
+import { Dispatch, SetStateAction } from "react";
 import BuzzerSoundSettings from "./BuzzerSoundSettings";
 import FinalRoundTitleChanger from "./FinalRoundTitleChanger";
 
-export default function AdminSettings({ game, setGame, send }) {
+interface AdminSettingsProps {
+  game: Game;
+  setGame: Dispatch<SetStateAction<Game>>;
+  send: (data: any) => void;
+}
+
+export default function AdminSettings({ game, setGame, send }: AdminSettingsProps) {
   return (
     <div className="flex flex-col items-center">
       <div className="grid grid-cols-2 gap-x-48 gap-y-10">
