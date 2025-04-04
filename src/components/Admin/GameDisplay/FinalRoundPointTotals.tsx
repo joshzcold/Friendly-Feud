1e570394dfcc983f95542f5a9f8aa190813144ff
Karl Romet Somelar <108818570+karlromets@users.noreply.github.com>
2025-04-04 21:11:50 +0300

    chore: FinalRoundPointTotals.jsx -> .tsx

--- src/components/Admin/GameDisplay/FinalRoundPointTotals.tsx
+++ src/components/Admin/GameDisplay/FinalRoundPointTotals.tsx
@@ -3,25 +3,18 @@ import { useTranslation } from "react-i18next";
 function FinalRoundPointTotalsTextFunction({ title, total, isFinalSecond, place }) {
   const { t } = useTranslation();
   const backgroundColor =
-    (isFinalSecond && place === 1) || (!isFinalSecond && place === 0)
-      ? "bg-primary-200"
-      : "bg-secondary-300";
+    (isFinalSecond && place === 1) || (!isFinalSecond && place === 0) ? "bg-primary-200" : "bg-secondary-300";
   return (
     <div
       className={`flex flex-row items-center space-x-2 rounded-3xl border-2 p-4 text-2xl text-foreground ${backgroundColor} text-foreground`}
     >
-      <p id={`finalRoundPointTotal${place}TitleText`}>
-        {t(title)}:{" "}
-      </p>
-      <p id={`finalRoundPointTotal${place}TotalText`}>
-        {total}
-      </p>
+      <p id={`finalRoundPointTotal${place}TitleText`}>{t(title)}: </p>
+      <p id={`finalRoundPointTotal${place}TotalText`}>{total}</p>
     </div>
   );
 }
 
-const calculateTotalPoints = (rounds) => 
-  rounds.reduce((total, round) => total + parseInt(round.points), 0);
+const calculateTotalPoints = (rounds) => rounds.reduce((total, round) => total + parseInt(round.points), 0);
 
 function FinalRoundPointTotals({ game }) {
   const roundOneTotal = calculateTotalPoints(game.final_round);
