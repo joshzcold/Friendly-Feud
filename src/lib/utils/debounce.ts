ac96d99d3f673ba180427badbe19109b9d9e144d
Karl Romet Somelar <108818570+karlromets@users.noreply.github.com>
2025-04-04 22:31:30 +0300

    refactor(debounce): migrate to TypeScript

--- src/lib/utils/debounce.ts
+++ src/lib/utils/debounce.ts
@@ -1,8 +1,11 @@
-export function debounce(callback, wait = 400) {
-  let timeout;
-  return (...args) => {
+type AnyFunction = (...args: any[]) => any;
+
+export function debounce<T extends AnyFunction>(callback: T, wait = 400): (...args: Parameters<T>) => void {
+  let timeout: ReturnType<typeof setTimeout> | undefined;
+
+  return (...args: Parameters<T>): void => {
     clearTimeout(timeout);
-    timeout = setTimeout(function () {
+    timeout = setTimeout(function (this: unknown) {
       callback.apply(this, args);
     }, wait);
   };
